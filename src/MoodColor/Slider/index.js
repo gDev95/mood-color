0d54f62adfd0690568ebde97df0958d483ed436f
Paaaaaaul <paulmeyerber@gmail.com>
2018-02-09 15:59:42 -0600

    slider made fully presentational

--- src/MoodColor/Slider/index.js
+++ src/MoodColor/Slider/index.js
@@ -6,65 +6,49 @@ import HappyIcon from 'material-ui/svg-icons/social/mood'
 import SadIcon from 'material-ui/svg-icons/social/mood-bad'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
+// change the slider default values of the material-ui theme
 
-
-
-class MoodSlider extends React.Component {
-	constructor(props){
-		super(props)
-		this.state = {
-			value:6
+const MoodSlider = (props) => {
+	const { mood, moodValue, changeMood} = props
+	const muiTheme = getMuiTheme({
+		slider: {
+			selectionColor: mood ? '#FFF' : '#000',
+			handleFillColor: mood ? '#FFF' : '#000'
 		}
-		this.onMoodChange = this.onMoodChange.bind(this)
-	}
-	onMoodChange (event,value) {
-		event.preventDefault()
-		this.props.changeMood(value)
-		this.setState({value: value})   
-	}
-	
-	render() {
-		// change the slider default values of the material-ui themeyarn
-		const muiTheme = getMuiTheme({
-			slider: {
-				selectionColor: this.props.mood ? '#FFF' : '#000',
-				handleFillColor: this.props.mood ? '#FFF' : '#000'
-			}
-		})
-		return(
-			<div className='slider-wrapper'>
-				<div className='slider-container'>
-					<HappyIcon 
-						className='mood-emotion' 
-						style={{color: this.props.mood ? '#FFF' : '#000'}}
-					/>
-					<div className='slider'>
-						<MuiThemeProvider muiTheme={muiTheme}>
-							<Slider  
-								min={0} 
-								max={12} 
-								value={this.state.value} 
-								onChange={this.onMoodChange} 
-							/>
-						</MuiThemeProvider>
-					</div>
-					<SadIcon 
-						className='mood-emotion' 
-						style={{color: this.props.mood ? '#FFF' : '#000'}}
-					/>
+	})
+	return (
+		<div className='slider-wrapper'>
+			<div className='slider-container'>
+				<HappyIcon 
+					className='mood-emotion' 
+					style={{color: mood ? '#FFF' : '#000'}}
+				/>
+				<div className='slider'>
+					<MuiThemeProvider muiTheme={muiTheme}>
+						<Slider  
+							min={0} 
+							max={12} 
+							value={moodValue} 
+							onChange={changeMood} 
+						/>
+					</MuiThemeProvider>
 				</div>
-				<span style={{color: this.props.mood ? '#FFF' : '#000'}}>  {this.props.mood ? `Your Mood: ${this.props.mood}` : ''} </span>
-			</div> 
-		)
-	}
+				<SadIcon 
+					className='mood-emotion' 
+					style={{color: mood ? '#FFF' : '#000'}}
+				/>
+			</div>
+			<span style={{color: mood ? '#FFF' : '#000', fontFamily: 'Kavivanar'}}>  {mood ? `Your Mood: ${mood}` : ''} </span>
+		</div> 
+	)
 }
+
 
 
 MoodSlider.propTypes = {
 	changeMood: PropTypes.func.isRequired,
 	mood: PropTypes.string,
-	moodColor: PropTypes.string,
-	moodFontColor: PropTypes.string
+	moodValue: PropTypes.number
 }
 
 export default MoodSlider
